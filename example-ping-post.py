2a00dd190ce5ae48ec144bc54bda3d79da71c376
Jeffrey Considine <jconsidi@gmail.com>
2021-02-21 22:21:32 -0500

    + simple example ping/post to solvedhome bidder

---
+++ example-ping-post.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import json
+import sys
+
+import requests
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Example ping/post flow.")
+    parser.add_argument("username", help="username for test ping/post")
+    parser.add_argument("password", help="password for test ping/post")
+
+    args = parser.parse_args()
+
+    ping_request_data = {
+        "lead_vertical": "windows",
+        "lead_data": {
+            "building_stories": 2,
+            "contact_tcpa_consent": True,
+            "windows_count": 3,
+            "property_zipcode": "20500",
+            "request_emergency": False,
+            "request_timestamp": str(datetime.datetime.now()),
+        },
+        "test": True,
+    }
+
+    ping_request_url = "https://solvedhome-bidder.herokuapp.com/v1/ping"
+
+    print("PING REQUEST:")
+    print(json.dumps(ping_request_data))
+    print("")
+
+    ping_response = requests.post(
+        ping_request_url,
+        auth=(args.username, args.password),
+        data=json.dumps(ping_request_data),
+    )
+    if ping_response.status_code != 200:
+        sys.stderr.write(
+            f"got unexpected ping HTTP status code {ping_response.status_code:d}\n"
+        )
+        return 1
+
+    print("PING RESPONSE:")
+    print(ping_response.text)
+    print("")
+
+    ping_response_data = json.loads(ping_response.text)
+
+    post_request_data = ping_request_data.copy()
+    post_request_data["lead_data"].update(
+        {
+            "contact_email": "test@solvedhome.test",
+            "contact_first_name": "Testy",
+            "contact_last_name": "Tester",
+            "contact_phone_mobile": "987-654-3210",
+            "contact_tcpa_consent": True,
+            "ping_uuid": ping_response_data["ping_uuid"],
+            "property_address": "1600 Pennsylvania Avenue NW",
+            "property_city": "Washington",
+            "property_state": "DC",
+        }
+    )
+
+    post_request_url = "https://solvedhome-bidder.herokuapp.com/v1/post"
+
+    print("POST REQUEST:")
+    print(json.dumps(post_request_data))
+    print("")
+
+    post_response = requests.post(
+        post_request_url,
+        auth=(args.username, args.password),
+        data=json.dumps(post_request_data),
+    )
+    if post_response.status_code != 200:
+        sys.stderr.write(
+            f"got unexpected post HTTP status code {post_response.status_code:d}\n"
+        )
+        return 1
+
+    print("POST RESPONSE:")
+    print(post_response.text)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
